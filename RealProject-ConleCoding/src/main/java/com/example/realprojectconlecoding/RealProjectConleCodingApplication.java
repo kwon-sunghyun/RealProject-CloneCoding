bbfb9387e705273b8f96128bbefba3261cfe12a2
BONOBONOYA1 <116361833+BONOBONOYA1@users.noreply.github.com>
2023-02-16 17:58:53 +0900

    feat : 실전프젝 클론코딩 중

---
+++ RealProject-ConleCoding/src/main/java/com/example/realprojectconlecoding/RealProjectConleCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.realprojectconlecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealProjectConleCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealProjectConleCodingApplication.class, args);
+    }
+
+}
